6e312652259f6c029caefc37e0c7fafdd97ed83e
kumar gaurav singh <you@example.com>
2024-03-01 09:16:27 +0530

    connect to mongo atlas

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME="tube";  // Name of your data base in mongo db.
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv"
+
+import connectDB from './db/index.js';
+
+
+dotenv.config({path:'./env'})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from 'express'
+const app=express()
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        app.on("error",()=>{
+            console.log("ERROR:",error)
+            throw new error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+        throw new  error
+    }
+})()
+*/
